9cbb90e2a3828f95d3b677c5ad5baec05bacce95
shalemrv <shalemrv@gmail.com>
2020-08-31 18:19:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express	= require('express');
+const path		= require('path');
+const app		= express();
 
 app.use(express.static(__dirname + '/dist/angular9-customers'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/angular9-customers/index.html'));});
+
+app.get('/*', (req,res)=>{
+	res.sendFile(path.join(__dirname + '/dist/angular9-customers/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
